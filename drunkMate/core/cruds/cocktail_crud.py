0067685181f43b1100f581f34c8f82f5b3e191d9
Rudovich Kirill <Rudovichkirill@gmail.com>
2022-12-26 14:28:32 +0300

    a

--- drunkMate/core/cruds/cocktail_crud.py
+++ drunkMate/core/cruds/cocktail_crud.py
@@ -94,7 +94,7 @@ async def delete_cocktail(cocktail_id: str):
         )
     
     for comment in cocktail['comments']:
-        repository.delete_comment(comment['_id'])
+        repository.delete_comment(str(comment['_id']))
     
     repository.delete_cocktail(cocktail_id)
     
