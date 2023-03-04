c1e4a38d2550ec646b95f53979cf4f7a2158b8bb
24eeeee <smyshlyaev1104@yandex.ru>
2023-03-04 21:01:51 +0300

    adaptation to front

--- drunkMate/api/auth/__init__.py
+++ drunkMate/api/auth/__init__.py
@@ -11,7 +11,7 @@ def bootstrap(app: FastAPI) -> FastAPI:
     ]
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=origins,
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
